489dad67a25c91d4821f25576fe5ef5d578cceba
Altyn Berdiyeva <aberdiyeva@lisaacademy.org>
2020-04-18 14:00:42 -0500

    some code is here

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+// import data from "./data";
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			players: "",
+		};
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/players").then((response) => {
+			console.log(response);
+			this.setState({
+				players: response.data,
+			});
+			// console.log(this.setState);
+		});
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<h1>Players</h1>
+				/* {this.state.players.map((player) => (
+					<li>{player}</li>
+				))}
+			</div>
+		);
+	}
 }
-
 export default App;
